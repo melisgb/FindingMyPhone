cae2d36e5eccade35c8ca712140000e0f75d8693
melisgb <gloriamelissa1@hotmail.com>
2020-06-16 23:12:14 +0100

    Reading from ContactsApp and loading into this app
    After checking for the permission granted it will show a list of all the contacts to add them into the app.

--- app/src/main/java/com/example/findingmyphone/MainActivity.kt
+++ app/src/main/java/com/example/findingmyphone/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.findingmyphone
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import java.security.Permission
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +22,6 @@ class MainActivity : AppCompatActivity() {
         val userData = UserData(this)
         userData.getPhoneNumber()
 
-
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -26,17 +31,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.it_addTracker ->{
+        when (item.itemId) {
+            R.id.it_addTracker -> {
                 val intent = Intent(this, TrackingActivity::class.java)
                 startActivity(intent)
             }
-            R.id.it_help ->{
+            R.id.it_help -> {
                 //TODO : ask friends for help
             }
             else -> return super.onOptionsItemSelected(item)
         }
         return true
     }
+
 
 }
--- app/src/main/java/com/example/findingmyphone/TrackingActivity.kt
+++ app/src/main/java/com/example/findingmyphone/TrackingActivity.kt
@@ -1,10 +1,16 @@
 package com.example.findingmyphone
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.*
 import android.widget.BaseAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_tracking.*
 import kotlinx.android.synthetic.main.contact_element.view.*
 
@@ -15,7 +21,8 @@ class TrackingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tracking)
 
-        generateDummyData()
+        //generateDummyData()
+        checkPermission()
 
         contactAdapter = ContactAdapter(this, contactsList)
         listView_contacts.adapter = contactAdapter
@@ -30,7 +37,7 @@ class TrackingActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.it_addContact -> {
-                //TODO: go to another activity
+                checkPermission()
             }
             R.id.it_finishActivity -> {
                 finish()
@@ -44,6 +51,83 @@ class TrackingActivity : AppCompatActivity() {
         contactsList.add(Contact("Melissa", "07383561085"))
         contactsList.add(Contact("Carlos", "07393901463"))
         contactsList.add(Contact("Lovie", "07393901420"))
+    }
+
+    //to get Contacts App info
+    val PERMISION_READ_CODE = 123
+    fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(android.Manifest.permission.READ_CONTACTS),
+                PERMISION_READ_CODE
+            )
+            return
+        }
+        chooseFromContacts()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISION_READ_CODE -> {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    chooseFromContacts()
+                }
+                else{
+                    Toast.makeText(applicationContext, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            }
+        }
+    }
+
+    val PICKED_IMG_CODE = 1111
+    fun chooseFromContacts() {
+        val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+        startActivityForResult(intent, PICKED_IMG_CODE)
+    }
+    //after the user pickes an image.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode){
+            PICKED_IMG_CODE -> {
+                if(resultCode == Activity.RESULT_OK){
+                    val contactData = data!!.data
+                    val cursor = contentResolver.query(contactData!!, null, null, null,null) //to get data from the contentProvider
+
+                    if(cursor!!.moveToFirst()){
+                        val contactId = cursor.getString(cursor.getColumnIndexOrThrow(
+                            ContactsContract.Contacts._ID))
+                        val hasPhone = cursor.getString(cursor.getColumnIndexOrThrow(
+                            ContactsContract.Contacts.HAS_PHONE_NUMBER))
+
+                        if(hasPhone.equals("1")){
+                            val phonesCursor = contentResolver.query(
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId, null, null)
+
+                            phonesCursor!!.moveToFirst()
+                            val phoneNumber = phonesCursor.getString(phonesCursor.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER))
+                            val name = phonesCursor.getString(phonesCursor.getColumnIndex(
+                                ContactsContract.Contacts.DISPLAY_NAME))
+                            contactsList.add(Contact(name, phoneNumber))
+                            contactAdapter!!.notifyDataSetChanged()
+                        }
+                    }
+
+                }
+            }
+            else -> {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
     }
 
     inner class ContactAdapter : BaseAdapter {
