9ea10a90dc2bdd16edd29c3a4810a12c9be41fcc
melisgb <gloriamelissa1@hotmail.com>
2020-06-16 23:15:58 +0100

    Putting ContactAdapter to a separate class

---
+++ app/src/main/java/com/example/findingmyphone/ContactAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.findingmyphone
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.contact_element.view.*
+
+class ContactAdapter : BaseAdapter {
+    var listOfContacts = ArrayList<Contact>()
+    var context : Context? = null
+
+    constructor(context : Context, listOfContacts : ArrayList<Contact>) {
+        this.context = context
+        this.listOfContacts = listOfContacts
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val contact = listOfContacts[position]
+        var inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val contactElemView = inflater.inflate(R.layout.contact_element, null)
+
+        contactElemView.txtView_contactName.text = contact.name
+        contactElemView.txtView_contactPhone.text = contact.phone
+
+        return contactElemView
+    }
+
+    override fun getItem(position: Int): Any {
+        return listOfContacts[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return listOfContacts.size
+    }
+
+}
--- app/src/main/java/com/example/findingmyphone/TrackingActivity.kt
+++ app/src/main/java/com/example/findingmyphone/TrackingActivity.kt
@@ -129,38 +129,4 @@ class TrackingActivity : AppCompatActivity() {
             }
         }
     }
-
-    inner class ContactAdapter : BaseAdapter {
-        var listOfContacts = ArrayList<Contact>()
-        var context : Context? = null
-
-        constructor(context : Context, listOfContacts : ArrayList<Contact>) {
-            this.context = context
-            this.listOfContacts = listOfContacts
-        }
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            val contact = listOfContacts[position]
-            var inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            val contactElemView = inflater.inflate(R.layout.contact_element, null)
-
-            contactElemView.txtView_contactName.text = contact.name
-            contactElemView.txtView_contactPhone.text = contact.phone
-
-            return contactElemView
-        }
-
-        override fun getItem(position: Int): Any {
-            return listOfContacts[position]
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getCount(): Int {
-            return listOfContacts.size
-        }
-
-    }
 }
