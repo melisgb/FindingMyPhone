2b02fdf8339ddaffdcc0ede9a4b0d5fe0165ef02
melisgb <gloriamelissa1@hotmail.com>
2020-06-17 21:17:43 +0100

    Showing the list of contacts in MainActivity

--- app/src/main/java/com/example/findingmyphone/MainActivity.kt
+++ app/src/main/java/com/example/findingmyphone/MainActivity.kt
@@ -8,13 +8,17 @@ import android.os.Bundle
 import android.provider.ContactsContract
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.AdapterView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 import java.security.Permission
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
+    var contactsList = ArrayList<Contact>()
+    var contactAdapt : ContactAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +26,17 @@ class MainActivity : AppCompatActivity() {
         val userData = UserData(this)
         userData.getPhoneNumber()
 
+        contactsList.add(Contact("Melissa", "07383561085"))
+        contactsList.add(Contact("Carlos", "07393901463"))
+        contactsList.add(Contact("Lovie", "07393901420"))
+
+
+        contactAdapt = ContactAdapter(this, contactsList)
+        listView_trackers.adapter = contactAdapt
+
+        listView_trackers.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val contactInfo = contactsList[position]
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
